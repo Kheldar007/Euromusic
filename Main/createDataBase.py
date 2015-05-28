902792b5ab746b1ed780b8b97cbd6236b574d67c
Isa <iundomiel@hotmail.fr>
2015-05-28 13:13:07 +0200

    Ajout du contenu pour le dossier Main.

--- Main/createDataBase.py
+++ Main/createDataBase.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python
+# encoding: utf=8
 
+# Cree une base de donnees details concernant les chansons contenues dans un
+# dossier.
+
+usage = """
+Usage: 
+    python createDataBase.py <path/>
+
+Example:
+    python createDataBase.py RepertoireDeChansons/
+"""
+
+
+if __name__ == '__main__':
+	import sys
+	
+	try:
+		folder = sys.argv[1]
+	except:
+		print usage
+		sys.exit(-1)
